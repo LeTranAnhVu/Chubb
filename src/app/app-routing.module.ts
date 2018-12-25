9417a3f0cef362999bca57e37fc590565582f992
Tinh Pham <mrkolikz@gmail.com>
2018-12-25 14:50:27 +0700

    upload_online5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from '../app/pages/about/about.component';
 import { Buyonline4Component } from '../app/pages/buyonline4/buyonline4.component';
 import { ProductComponent } from '../app/pages/product/product.component';
+import { Buyonline5Component } from './pages/buyonline5/buyonline5.component';
 
 
 
@@ -12,7 +13,7 @@ import { ProductComponent } from '../app/pages/product/product.component';
 const routes: Routes = [
     {path: 'about' , component: AboutComponent},
     {path: 'product' , component: ProductComponent},
-    {path: 'buyonline4' , component: Buyonline4Component},
+    {path: 'buyonline4' , component: Buyonline4Component},{path: 'buyonline5' , component: Buyonline5Component}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
