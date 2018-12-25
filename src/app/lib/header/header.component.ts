a75c2215ac84138c2f08cc32c282018d4d635b65
Tinh Pham <mrkolikz@gmail.com>
2018-12-25 09:47:21 +0700

    add header ts

--- src/app/lib/header/header.component.ts
+++ src/app/lib/header/header.component.ts
@@ -11,5 +11,12 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  openMenu() {
+    let menuBtn = document.getElementById('header__trigger');
+    let header__menu = document.getElementById('header__menu');
+    let header__overlay = document.getElementById('header__overlay');
+    menuBtn.classList.toggle('header__trigger--open');
+    header__menu.classList.toggle('header__menu--active');
+    header__overlay.classList.toggle('header__overlay--active');
+  }
 }
