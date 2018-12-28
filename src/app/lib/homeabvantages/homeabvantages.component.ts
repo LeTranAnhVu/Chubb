0837431fbedcc689c8ed78a7f901b7926a25d395
Nguyen Sang <nnsang24@gmail.com>
2018-12-29 00:53:17 +0700

    change product to json

--- src/app/lib/homeabvantages/homeabvantages.component.ts
+++ src/app/lib/homeabvantages/homeabvantages.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AbvantageHome} from '../../interfaces/abvantage-home';
+import {HomeabvantagesService} from '../../services/homeabvantages/homeabvantages.service';
 @Component({
-  selector: 'app-homeabvantages',
-  templateUrl: './homeabvantages.component.html',
-  styleUrls: ['./homeabvantages.component.sass']
+	selector: 'app-homeabvantages',
+	templateUrl: './homeabvantages.component.html',
+	styleUrls: ['./homeabvantages.component.sass']
 })
 export class HomeabvantagesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	constructor(private abvantageHome: HomeabvantagesService) { }
+	abvantagehome: AbvantageHome[];
+	ngOnInit() {
+		this.abvantageHome.getData().subscribe(data => {
+			this.abvantagehome = data.homeabvantages;
+		});
+	}
 
 }
