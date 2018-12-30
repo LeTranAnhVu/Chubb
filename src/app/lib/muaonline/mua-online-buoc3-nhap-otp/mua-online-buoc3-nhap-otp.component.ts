a30587081e181994ee27fa05c53b5ef60303c2e8
LeTranAnhVu <vule5726@gmail.com>
2018-12-30 20:56:49 +0700

    refactor the forms done

--- src/app/lib/muaonline/mua-online-buoc3-nhap-otp/mua-online-buoc3-nhap-otp.component.ts
+++ src/app/lib/muaonline/mua-online-buoc3-nhap-otp/mua-online-buoc3-nhap-otp.component.ts
@@ -3,27 +3,26 @@ import { Router } from '@angular/router';
 import { BannerContent_2 } from '../../../interfaces/banner-content-2';
 
 @Component({
-	selector: 'app-mua-online-buoc1',
-	templateUrl: './mua-online-buoc1.component.html',
-	styleUrls: ['./mua-online-buoc1.component.sass']
+	selector: 'app-mua-online-buoc3-nhap-otp',
+	templateUrl: './mua-online-buoc3-nhap-otp.component.html',
+	styleUrls: ['./mua-online-buoc3-nhap-otp.component.sass']
 })
-export class MuaOnlineBuoc1Component implements OnInit {
+export class MuaOnlineBuoc3NhapOtpComponent implements OnInit {
 
 	constructor(private router: Router) { }
 	bannerContent: BannerContent_2 = {
-		title: 'Tính Phí',
+		title: 'Xác nhận mã OTP',
 		subTitle: 'Các bước mua online'
 	};
 	nextStep(): void {
 		// console.log('not doing');
-		this.router.navigate(['/mua-online/mua-online-buoc2']);
+		this.router.navigate(['/mua-online/mua-online-buoc4']);
 	}
 	backStep(): void {
-		this.router.navigate(['/mua-online/mua-online-buoc1']);
+		this.router.navigate(['/mua-online/mua-online-buoc3']);
 
 	}
 
 	ngOnInit() {
 	}
-
 }
